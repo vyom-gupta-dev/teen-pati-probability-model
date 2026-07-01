a7835fd0ed148b6e534de7d07ecd6cd81dedfbfe
vyom-gupta-dev <vyomid@gmail.com>
2026-07-02 05:03:23 +0530

    Add files via upload

---
+++ src/data.py
@@ -0,0 +1,34 @@
+#This module contains all the data that is required for the Setup module
+
+#Data contains all possible hands
+#Each hand is of form [Higher,Middle,Lower]
+data = []
+for high in range(2, 15):
+    for mid in range(2, high + 1):
+        for low in range(2, mid + 1):
+            data.append([high, mid, low])
+
+#Find numeric Values
+CardValues = {
+    'A' : 14,
+    'K' : 13,
+    'Q' : 12,
+    'J' : 11
+}
+
+#Card Counter
+CardCount = {
+    14 : 4,
+    13 : 4,
+    12 : 4,
+    11 : 4,
+    10 : 4,
+    9 : 4,
+    8 : 4,
+    7 : 4,
+    6 : 4,
+    5 : 4,
+    4 : 4,
+    3 : 4,
+    2 : 4
+}
