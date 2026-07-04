aea995707d10e2c455cf3ce2594206599a81ea8c
vyom-gupta-dev <vyomid@gmail.com>
2026-07-04 20:57:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from Setup import *
+
+print("RUBRIC - VALUE INPUTTING")
+print("Press A for Ace")
+print("Press K for King")
+print("Press Q for Queen")
+print("Press J for Jack")
+print("For Numbered Card, enter the number \n")
+
+print("RUBRIC - SUIT INPUTTING")
+print("Press C for Clubs")
+print("Press S for Spades")
+print("Press H for Hearts")
+print("Press D for Diamonds \n")
+
+print("INPUTS:")
+Start()
+
+
+choice = input("\nPress any key to exit").upper()
+exit()
+    
